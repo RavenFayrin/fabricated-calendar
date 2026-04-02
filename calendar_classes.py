f18e745f831702ca69a91a7e747872c7ccb67ae5
RavenFayrin <raven.fayrin@gmail.com>
2026-04-02 18:53:04 -0400

    Wrote base calendar classes

--- calendar_classes.py
+++ calendar_classes.py
@@ -1,4 +1,17 @@
-class Month:
-    def __init__(self, name):
+class WeekDay:
+    def __init__(self, name, order):
         self.name = name
-        
+        self.order = order
+
+class Month:
+    def __init__(self, name, order, num_days):
+        self.name = name
+        self.order = order
+        self.num_days = num_days
+
+class Era:
+    def __init__(self, name, order, start_year, end_year):
+        self.name = name
+        self.order = order
+        self.start_year = start_year
+        self.end_year = end_year
