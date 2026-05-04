5d004a51fac47c755606530863e1148e1e11fd00
RavenFayrin <raven.fayrin@gmail.com>
2026-05-04 19:56:10 -0400

    resloves #1, #2, #3

--- calendar_classes.py
+++ calendar_classes.py
@@ -1,17 +1,21 @@
 class WeekDay:
-    def __init__(self, name, order):
+    def __init__(self, id, name, order):
+        self.id = id
         self.name = name
         self.order = order
 
 class Month:
-    def __init__(self, name, order, num_days):
+    def __init__(self, id, name, order, num_days):
+        self.id = id
         self.name = name
         self.order = order
         self.num_days = num_days
 
 class Era:
-    def __init__(self, name, order, start_year, end_year):
+    def __init__(self, id, name, order, start_year, end_year):
+        self.id = id
         self.name = name
         self.order = order
         self.start_year = start_year
         self.end_year = end_year
+        self.length = end_year - start_year + 1
