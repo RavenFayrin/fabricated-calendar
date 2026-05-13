998b12cbfcdfedb68940d874554ef3e78d27b65a
Lydia Bollig <96959690+RavenFayrin@users.noreply.github.com>
2026-05-13 13:41:05 -0400

    Add CalendarSystem and Date classes

--- calendar_classes.py
+++ calendar_classes.py
@@ -1,3 +1,10 @@
+class CalendarSystem:
+    def __init__(self, name, weekdays, months, eras):
+        self.name = name
+        self.weekdays = weekdays
+        self.months = months
+        self.eras = eras
+        
 class WeekDay:
     def __init__(self, id, name, order):
         self.id = id
@@ -12,10 +19,16 @@ class Month:
         self.num_days = num_days
 
 class Era:
-    def __init__(self, id, name, order, start_year, end_year):
+    def __init__(self, id, name, order, start_year):
         self.id = id
         self.name = name
         self.order = order
         self.start_year = start_year
-        self.end_year = end_year
-        self.length = end_year - start_year + 1
+
+class Date:
+    def __init__(self, calendar_system, era, year, month, day):
+        self.calendar_system = calendar_system
+        self.era = era
+        self.year = year
+        self.month = month
+        self.day = day
