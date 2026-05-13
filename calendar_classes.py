f0a7c208f9fa114f867306babfbf5777af7f9dbf
RavenFayrin <raven.fayrin@gmail.com>
2026-05-13 19:49:08 -0400

    Added date to num calculations

--- calendar_classes.py
+++ calendar_classes.py
@@ -1,38 +1,36 @@
- class WeekDay:
-    def __init__(self, id, name, order):
-        self.id = id
+class WeekDay:
+    def __init__(self, name, order):
         self.name = name
         self.order = order
 
 class Month:
-    def __init__(self, id, name, order, num_days):
-        self.id = id
+    def __init__(self, name, order, num_days):
         self.name = name
         self.order = order
         self.num_days = num_days
 
-class Era:
-    def __init__(self, id, name, order, start_year):
-        self.id = id
-        self.name = name
-        self.order = order
-        self.start_year = start_year
-
 class CalendarSystem:
-    def __init__(self, name, weekdays, months, eras):
+    def __init__(self, name, weekdays, months):
         self.name = name
         self.weekdays = weekdays
         self.months = months
-        self.eras = eras
 
-    #def date_to_num(self):
-
+    def date_to_num(self, date):
+        days_in_year = 0
+        for month in self.months:
+            days_in_year += month.num_days
+        num = date.year * days_in_year
+        for month in self.months:
+            if month.order < date.month:
+                num += month.num_days
+        num += date.day
+        return num
+    
     #def num_to_date(self):
 
 class Date:
-    def __init__(self, calendar_system, era, year, month, day):
+    def __init__(self, calendar_system, year, month, day):
         self.calendar_system = calendar_system
-        self.era = era
         self.year = year
         self.month = month
         self.day = day
