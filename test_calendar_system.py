b31919bf5c8f8094baa560b9e04ef7f452543d40
Lydia Bollig <96959690+RavenFayrin@users.noreply.github.com>
2026-05-15 11:25:38 -0400

    Update test cases for CalendarSystem with new dynamic dates

--- test_calendar_system.py
+++ test_calendar_system.py
@@ -8,71 +8,69 @@ class TestCalendarSystem(unittest.TestCase):
         self.calendar_system = CalendarSystem(
             "Test Calendar", 
             [
-                WeekDay("Setacyde", 1),
-                WeekDay("M’férst", 2),
-                WeekDay("Teztor", 3),
-                WeekDay("Wysellor", 4),
-                WeekDay("TanéRhéim", 5),
-                WeekDay("Fedrist", 6),
-                WeekDay("Scynthay", 7)
+                WeekDay("WD1", 1),
+                WeekDay("WD2", 2),
+                WeekDay("WD3", 3),
+                WeekDay("WD4", 4),
+                WeekDay("WD5", 5),
+                WeekDay("WD6", 6),
+                WeekDay("WD7", 7)
                 ],
             [
-                Month("First", 1, 30),
-                Month("Second", 2, 30),
-                Month("Third", 3, 30),
-                Month("Fourth", 4, 30),
-                Month("Fifth", 5, 30),
-                Month("Sixth", 6, 30),
-                Month("Seventh", 7, 30),
-                Month("Eigth", 8, 30),
-                Month("Ninth", 9, 30),
-                Month("Tenth", 10, 30),
-                Month("Eleventh", 11, 30),
-                Month("Twelfth", 12, 30)
+                Month("M1", 1, 31),
+                Month("M2", 2, 28),
+                Month("M3", 3, 31),
+                Month("M4", 4, 30),
+                Month("M5", 5, 31),
+                Month("M6", 6, 30),
+                Month("M7", 7, 31),
+                Month("M8", 8, 31),
+                Month("M9", 9, 30),
+                Month("M10", 10, 31),
+                Month("M11", 11, 30),
+                Month("M12", 12, 31)
                 ]
             )
 
     def test_date_to_absolute_day(self):
-        date = Date(self.calendar_system, 2506, 6, 7)
+        date = Date(self.calendar_system, 2026, 1, 1)
         absolute_day = self.calendar_system.date_to_absolute_day(date)
-        self.assertEqual(absolute_day, 2506 * 360 + 150 + 7)
+        self.assertEqual(absolute_day, 2026 * 365 + 1)
 
     def test_absolute_day_to_date(self):
-        absolute_day = 2506 * 360 + 150 + 7
+        absolute_day = 2026 * 365 + 1
         date = self.calendar_system.absolute_day_to_date(absolute_day)
-        self.assertEqual(date.year, 2506)
-        self.assertEqual(date.month, 6)
-        self.assertEqual(date.day, 7)
+        self.assertEqual(date, Date(self.calendar_system, 2026, 1, 1)
 
     def test_get_num_days(self):
-        early_date = Date(self.calendar_system, 2506, 6, 7)
-        later_date = Date(self.calendar_system, 2507, 9, 16)
+        early_date = Date(self.calendar_system, 2025, 6, 7)
+        later_date = Date(self.calendar_system, 2026, 9, 16)
         days_between = self.calendar_system.get_num_days(early_date, later_date)
-        self.assertEqual(days_between, 459)
+        self.assertEqual(days_between, 466)
 
     def test_sort_dates(self):
         date_list = [
-            Date(self.calendar_system, 2507, 8, 3),
-            Date(self.calendar_system, 2506, 6, 7),
-            Date(self.calendar_system, 2507, 9, 16),
-            Date(self.calendar_system, 2498, 3, 25),
-            Date(self.calendar_system, 2507, 9, 27)
+            Date(self.calendar_system, 2026, 12, 25),
+            Date(self.calendar_system, 2025, 7, 4),
+            Date(self.calendar_system, 2026, 1, 17),
+            Date(self.calendar_system, 2026, 12, 24),
+            Date(self.calendar_system, 1973, 11, 28)
         ]
         sorted_date_list = self.calendar_system.sort_dates(date_list)
         
         self.assertEqual(sorted_date_list, [
-            Date(self.calendar_system, 2498, 3, 25),
-            Date(self.calendar_system, 2506, 6, 7),
-            Date(self.calendar_system, 2507, 8, 3),
-            Date(self.calendar_system, 2507, 9, 16),
-            Date(self.calendar_system, 2507, 9, 27)
+            Date(self.calendar_system, 1973, 11, 28),
+            Date(self.calendar_system, 2025, 7, 4),
+            Date(self.calendar_system, 2026, 1, 17),
+            Date(self.calendar_system, 2026, 12, 24),
+            Date(self.calendar_system, 2026, 12, 25)
             ]
         )
 
     def test_get_weekday_name(self):
         date = Date(self.calendar_system, 0, 1, 3)
         weekday_name = self.calendar_system.get_weekday_name(date)
-        self.assertEqual(weekday_name, "Teztor")
+        self.assertEqual(weekday_name, "WD3")
 
 if __name__ == '__main__':
     unittest.main()
