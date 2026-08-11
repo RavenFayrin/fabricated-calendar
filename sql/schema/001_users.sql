579c9b4e055f6a22deab0f37e7e37eda808d8e2b
RavenFayrin <raven.fayrin@gmail.com>
2026-08-10 20:40:57 -0400

    updated users table to function

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
 -- +goose Down
