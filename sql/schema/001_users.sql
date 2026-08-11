0fdcbb3e012e77a6fe359c66810a50a5171a9478
RavenFayrin <raven.fayrin@gmail.com>
2026-08-10 20:15:10 -0400

    added hashed password to database

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    email TEXT NOT NULL UNIQUE
 );
 
 -- +goose Down
