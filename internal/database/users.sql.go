0478a1bf9f87bfe092aeb5ff2060e8f39862f732
RavenFayrin <raven.fayrin@gmail.com>
2026-08-15 12:01:36 -0400

    added delete for user

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -7,6 +7,8 @@ package database
 
 import (
 	"context"
+
+	"github.com/google/uuid"
 )
 
 const createUser = `-- name: CreateUser :one
@@ -40,6 +42,16 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.UpdatedAt,
 	)
 	return i, err
+}
+
+const deleteUser = `-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1
+`
+
+func (q *Queries) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	_, err := q.db.ExecContext(ctx, deleteUser, id)
+	return err
 }
 
 const getUserByUsername = `-- name: GetUserByUsername :one
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -10,6 +10,10 @@ VALUES (
 )
 RETURNING *;
 
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
+
 -- name: GetUserByUsername :one
 SELECT * FROM users
 WHERE username = $1;
