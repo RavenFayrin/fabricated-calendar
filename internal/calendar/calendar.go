e7da68776f0662fcd7fcb93619f38522f72d43f6
RavenFayrin <raven.fayrin@gmail.com>
2026-08-18 22:07:07 -0400

    added database create calendar

--- internal/calendar/calendar.go
+++ internal/calendar/calendar.go
@@ -1,118 +1,33 @@
 package calendar
 
 import (
-	"fmt"
-	"sort"
+	"context"
+	"database/sql"
+	"fabricated-calendar/config"
+	"fabricated-calendar/internal/database"
+
+	"github.com/google/uuid"
 )
 
-type CalendarSystem struct {
-	Name     string
-	Weekdays []*WeekDay
-	Months   []*Month
-}
+func CreateCalendar(cfg config.Config, name, desc string, userID uuid.UUID) error {
+	var val bool
 
-func (cs *CalendarSystem) DaysInYear() int {
-
-	total := 0
-
-	for _, m := range cs.Months {
-		total += m.NumDays
+	if desc == "" {
+		val = false
+	} else {
+		val = true
 	}
 
-	return total
-}
-
-func (cs *CalendarSystem) WeeksInYear() int {
-	return cs.DaysInYear() / len(cs.Weekdays)
-}
-
-func (cs *CalendarSystem) DateToAbsoluteDay(date *Date) int {
-
-	absolute := date.Year * cs.DaysInYear()
-
-	for _, m := range cs.Months {
-		if m.Order < date.Month.Order {
-			absolute += m.NumDays
-		}
-	}
-
-	absolute += date.Day
-
-	return absolute
-}
-
-func (cs *CalendarSystem) AbsoluteDayToDate(absolute_day int) (*Date, error) {
-
-	daysInYear := cs.DaysInYear()
-
-	year := absolute_day / daysInYear
-	absolute_day %= daysInYear
-
-	month := 1
-
-	for _, m := range cs.Months {
-
-		if absolute_day >= m.NumDays {
-			absolute_day -= m.NumDays
-			month++
-		} else {
-			break
-		}
-	}
-
-	return NewDate(cs, year, month, absolute_day)
-}
-
-func (cs *CalendarSystem) WeekdayName(date *Date) string {
-
-	absolute := cs.DateToAbsoluteDay(date)
-
-	remainder := (absolute % len(cs.Weekdays)) - 1
-
-	if remainder < 0 {
-		remainder += len(cs.Weekdays)
-	}
-
-	return cs.Weekdays[remainder].Name
-}
-
-func (cs *CalendarSystem) SortDates(dates []*Date) {
-	sort.Slice(dates, func(i, j int) bool {
-		return dates[i].Before(dates[j])
+	_, err := cfg.DB.CreateCalendar(context.Background(), database.CreateCalendarParams{
+		Name: name,
+		Description: sql.NullString{
+			String: desc,
+			Valid:  val},
+		UserID: userID,
 	})
-}
-
-func (cs *CalendarSystem) DaysBetween(a, b *Date) int {
-	return cs.DateToAbsoluteDay(b) - cs.DateToAbsoluteDay(a)
-}
-
-func (cs *CalendarSystem) FormattedTimeBetween(a, b *Date) string {
-
-	years := 0
-	months := 0
-	days := 0
-
-	diff := cs.DaysBetween(a, b)
-
-	years = diff / cs.DaysInYear()
-	diff %= cs.DaysInYear()
-
-	days += a.Month.NumDays - a.Day
-	diff -= a.Month.NumDays - a.Day
-
-	days += b.Day
-	diff -= b.Day
-
-	for _, m := range cs.Months[a.Month.Order:] {
-
-		if diff >= m.NumDays {
-			diff -= m.NumDays
-			months++
-		} else {
-			break
-		}
+	if err != nil {
+		return err
 	}
 
-	return fmt.Sprintf("%d year(s), %d month(s), %d day(s)",
-		years, months, days)
+	return nil
 }
