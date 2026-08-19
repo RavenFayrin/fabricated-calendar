7e9807d53f5226f033094f5410613baf1148c973
RavenFayrin <raven.fayrin@gmail.com>
2026-08-18 22:05:42 -0400

    added database create weekday

--- internal/calendar/weekday.go
+++ internal/calendar/weekday.go
@@ -1,6 +1,22 @@
 package calendar
 
-type WeekDay struct {
-	Name  string
-	Order int
+import (
+	"context"
+	"fabricated-calendar/config"
+	"fabricated-calendar/internal/database"
+
+	"github.com/google/uuid"
+)
+
+func CreateWeekday(cfg config.Config, name string, order int32, calendardID, userID uuid.UUID) error {
+	_, err := cfg.DB.CreateWeekday(context.Background(), database.CreateWeekdayParams{
+		Name:       name,
+		DayOrder:   order,
+		CalendarID: calendardID,
+		UserID:     userID,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
