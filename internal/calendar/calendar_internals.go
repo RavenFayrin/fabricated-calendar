07d1f364e6de52b1638d4d63b073dc34138729be
RavenFayrin <raven.fayrin@gmail.com>
2026-08-18 22:06:21 -0400

    commented out internal functions for later use

---
+++ internal/calendar/calendar_internals.go
@@ -0,0 +1,118 @@
+package calendar
+
+// import (
+// 	"fmt"
+// 	"sort"
+// )
+
+// type CalendarSystem struct {
+// 	Name     string
+// 	Weekdays []*WeekDay
+// 	Months   []*Month
+// }
+
+// func (cs *CalendarSystem) DaysInYear() int {
+
+// 	total := 0
+
+// 	for _, m := range cs.Months {
+// 		total += m.NumDays
+// 	}
+
+// 	return total
+// }
+
+// func (cs *CalendarSystem) WeeksInYear() int {
+// 	return cs.DaysInYear() / len(cs.Weekdays)
+// }
+
+// func (cs *CalendarSystem) DateToAbsoluteDay(date *Date) int {
+
+// 	absolute := date.Year * cs.DaysInYear()
+
+// 	for _, m := range cs.Months {
+// 		if m.Order < date.Month.Order {
+// 			absolute += m.NumDays
+// 		}
+// 	}
+
+// 	absolute += date.Day
+
+// 	return absolute
+// }
+
+// func (cs *CalendarSystem) AbsoluteDayToDate(absolute_day int) (*Date, error) {
+
+// 	daysInYear := cs.DaysInYear()
+
+// 	year := absolute_day / daysInYear
+// 	absolute_day %= daysInYear
+
+// 	month := 1
+
+// 	for _, m := range cs.Months {
+
+// 		if absolute_day >= m.NumDays {
+// 			absolute_day -= m.NumDays
+// 			month++
+// 		} else {
+// 			break
+// 		}
+// 	}
+
+// 	return NewDate(cs, year, month, absolute_day)
+// }
+
+// func (cs *CalendarSystem) WeekdayName(date *Date) string {
+
+// 	absolute := cs.DateToAbsoluteDay(date)
+
+// 	remainder := (absolute % len(cs.Weekdays)) - 1
+
+// 	if remainder < 0 {
+// 		remainder += len(cs.Weekdays)
+// 	}
+
+// 	return cs.Weekdays[remainder].Name
+// }
+
+// func (cs *CalendarSystem) SortDates(dates []*Date) {
+// 	sort.Slice(dates, func(i, j int) bool {
+// 		return dates[i].Before(dates[j])
+// 	})
+// }
+
+// func (cs *CalendarSystem) DaysBetween(a, b *Date) int {
+// 	return cs.DateToAbsoluteDay(b) - cs.DateToAbsoluteDay(a)
+// }
+
+// func (cs *CalendarSystem) FormattedTimeBetween(a, b *Date) string {
+
+// 	years := 0
+// 	months := 0
+// 	days := 0
+
+// 	diff := cs.DaysBetween(a, b)
+
+// 	years = diff / cs.DaysInYear()
+// 	diff %= cs.DaysInYear()
+
+// 	days += a.Month.NumDays - a.Day
+// 	diff -= a.Month.NumDays - a.Day
+
+// 	days += b.Day
+// 	diff -= b.Day
+
+// 	for _, m := range cs.Months[a.Month.Order:] {
+
+// 		if diff >= m.NumDays {
+// 			diff -= m.NumDays
+// 			months++
+// 		} else {
+// 			break
+// 		}
+// 	}
+
+// 	return fmt.Sprintf("%d year(s), %d month(s), %d day(s)",
+// 		years, months, days)
+// }
--- internal/calendar/date.go
+++ internal/calendar/date.go
@@ -1,65 +1,65 @@
 package calendar
 
-import "fmt"
+// import "fmt"
 
-type Date struct {
-	Calendar *CalendarSystem
-	Year     int
-	Month    *Month
-	Day      int
-}
+// type Date struct {
+// 	Calendar *CalendarSystem
+// 	Year     int
+// 	Month    *Month
+// 	Day      int
+// }
 
-func NewDate(cal *CalendarSystem, year int, month interface{}, day int) (*Date, error) {
-	m, err := cal.GetMonth(month)
-	if err != nil {
-		return nil, err
-	}
+// func NewDate(cal *CalendarSystem, year int, month interface{}, day int) (*Date, error) {
+// 	m, err := cal.GetMonth(month)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &Date{
-		Calendar: cal,
-		Year:     year,
-		Month:    m,
-		Day:      day,
-	}, nil
-}
+// 	return &Date{
+// 		Calendar: cal,
+// 		Year:     year,
+// 		Month:    m,
+// 		Day:      day,
+// 	}, nil
+// }
 
-func (d *Date) Equals(other *Date) bool {
-	if other == nil {
-		return false
-	}
+// func (d *Date) Equals(other *Date) bool {
+// 	if other == nil {
+// 		return false
+// 	}
 
-	return d.Calendar == other.Calendar &&
-		d.Year == other.Year &&
-		d.Month == other.Month &&
-		d.Day == other.Day
-}
+// 	return d.Calendar == other.Calendar &&
+// 		d.Year == other.Year &&
+// 		d.Month == other.Month &&
+// 		d.Day == other.Day
+// }
 
-func (cs *CalendarSystem) GetMonth(month interface{}) (*Month, error) {
+// func (cs *CalendarSystem) GetMonth(month interface{}) (*Month, error) {
 
-	switch type_value := month.(type) {
+// 	switch type_value := month.(type) {
 
-	case string:
-		for _, m := range cs.Months {
-			if m.Name == type_value {
-				return m, nil
-			}
-		}
+// 	case string:
+// 		for _, m := range cs.Months {
+// 			if m.Name == type_value {
+// 				return m, nil
+// 			}
+// 		}
 
-	case int:
-		for _, m := range cs.Months {
-			if m.Order == type_value {
-				return m, nil
-			}
-		}
-	}
+// 	case int:
+// 		for _, m := range cs.Months {
+// 			if m.Order == type_value {
+// 				return m, nil
+// 			}
+// 		}
+// 	}
 
-	return nil, fmt.Errorf("invalid month")
-}
+// 	return nil, fmt.Errorf("invalid month")
+// }
 
-func (d *Date) AbsoluteDay() int {
-	return d.Calendar.DateToAbsoluteDay(d)
-}
+// func (d *Date) AbsoluteDay() int {
+// 	return d.Calendar.DateToAbsoluteDay(d)
+// }
 
-func (d *Date) Before(other *Date) bool {
-	return d.AbsoluteDay() < other.AbsoluteDay()
-}
+// func (d *Date) Before(other *Date) bool {
+// 	return d.AbsoluteDay() < other.AbsoluteDay()
+// }
