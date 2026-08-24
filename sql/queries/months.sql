623028f3ce25e0c3b59bb528ba84ab513eca50bd
RavenFayrin <raven.fayrin@gmail.com>
2026-08-24 12:00:55 -0400

    added update for month

--- sql/queries/months.sql
+++ sql/queries/months.sql
@@ -12,12 +12,18 @@ VALUES (
 )
 RETURNING *;
 
--- name: DeleteMonth :exec
-DELETE FROM month
-WHERE id = $1;
-
 -- name: GetMonthsByCalendarId :many
 SELECT *
 FROM month
 WHERE calendar_id = $1
 ORDER BY month_order ASC;
+
+-- name: UpdateCalendarById :one
+UPDATE month
+SET name = $1, month_order = $2, days_in_month = $3, updated_at = NOW()
+WHERE id = $4
+RETURNING *;
+
+-- name: DeleteMonth :exec
+DELETE FROM month
+WHERE id = $1;
