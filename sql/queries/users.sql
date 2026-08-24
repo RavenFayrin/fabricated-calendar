ac8bb9a880a7d1105ab3547987c4ccf41cc9043f
RavenFayrin <raven.fayrin@gmail.com>
2026-08-24 12:03:06 -0400

    added update call

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -10,10 +10,16 @@ VALUES (
 )
 RETURNING *;
 
--- name: DeleteUser :exec
-DELETE FROM users
-WHERE id = $1;
-
 -- name: GetUserByUsername :one
 SELECT * FROM users
 WHERE username = $1;
+
+-- name: UpdateUserById :one
+UPDATE users
+SET username = $1, hashed_password = $2, email = $3, updated_at = NOW()
+WHERE id = $4
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
