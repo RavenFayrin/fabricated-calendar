55aa09d520856b6c2edcbcc740812c54a61ad49e
RavenFayrin <raven.fayrin@gmail.com>
2026-08-26 16:54:22 -0400

    added create month - fixes #42

--- internal/calendar/month.go
+++ internal/calendar/month.go
@@ -1,6 +1,26 @@
 package calendar
 
-// create
+import (
+	"context"
+	"fabricated-calendar/config"
+	"fabricated-calendar/internal/database"
+
+	"github.com/google/uuid"
+)
+
+func CreateMonth(cfg config.Config, name string, order, numDays int32, calendardID, userID uuid.UUID) error {
+	_, err := cfg.DB.CreateMonth(context.Background(), database.CreateMonthParams{
+		Name:        name,
+		MonthOrder:  order,
+		DaysInMonth: numDays,
+		CalendarID:  calendardID,
+		UserID:      userID,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // retrive
 
