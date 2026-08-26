159ac8557ad2f82b7c41d2520377706698b062b7
RavenFayrin <raven.fayrin@gmail.com>
2026-08-26 16:38:25 -0400

    added funciton to update weekdays - relsolves #33

--- internal/calendar/weekday.go
+++ internal/calendar/weekday.go
@@ -8,15 +8,31 @@ import (
 	"github.com/google/uuid"
 )
 
-func CreateWeekday(cfg config.Config, name string, order int32, calendardID, userID uuid.UUID) error {
+func CreateWeekday(cfg config.Config, name string, order int32, calendardID, weekdayID uuid.UUID) error {
 	_, err := cfg.DB.CreateWeekday(context.Background(), database.CreateWeekdayParams{
 		Name:       name,
 		DayOrder:   order,
 		CalendarID: calendardID,
-		UserID:     userID,
+		UserID:     weekdayID,
 	})
 	if err != nil {
 		return err
 	}
 	return nil
 }
+
+// Retrive
+
+func UpdateWeekday(cfg config.Config, name string, order int32, weekdayID uuid.UUID) error {
+	_, err := cfg.DB.UpdateWeekdayById(context.Background(), database.UpdateWeekdayByIdParams{
+		Name:     name,
+		DayOrder: order,
+		ID:       weekdayID,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete
