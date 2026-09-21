3863ede469e7770315f1f19f6837240b15a1665d
RavenFayrin <raven.fayrin@gmail.com>
2026-09-21 19:30:38 -0400

    Refactor nullStringIdentifier function for clarity and simplicity

--- internal/calendar/supporting_func.go
+++ internal/calendar/supporting_func.go
@@ -11,3 +11,14 @@ func stringToInt32(order string) (int32, error) {
 	val32 := int32(val64)
 	return val32, nil
 }
+
+func nullStringIdentifier(text string) bool {
+	var val bool
+
+	if text == "" {
+		val = false
+	} else {
+		val = true
+	}
+	return val
+}
